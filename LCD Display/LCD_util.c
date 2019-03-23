0622dccfffb0b75e1e88ae08e41befb6819cc7be
Cody Beumer <cbeumer0428@gmail.com>
2019-03-23 00:49:49 -0500

    LCD
    1

--- LCD Display/LCD_util.c
+++ LCD Display/LCD_util.c
@@ -1,5 +1,6 @@
 #include "stm32l4xx_hal.h"
 #include "LCD_util.h"
+#include "USART1_util.h"
 
 #define LCD_WRITE_ADDRESS 0x4E
 #define LCD_CLEAR_DISPLAY 0x01
@@ -11,48 +12,48 @@
 #define LCD_DISPLAY_OFF_CURSOR_OFF 0x08
 #define LCD_DISPLAY_OFF_CURSOR_ON 0x0A
 #define LCD_DISPLAY_ON_CURSOR_OFF 0x0C
-0x0E
-0x0F
-0x10
-0x14
-0x18
-0x1C
 
 
 
 
-unsigned char buffer[4];
+uint8_t buffer[4];
 
 void LCD_init(I2C_HandleTypeDef i2cHandle) {
-	lcd_send_cmd(0x02); //DB1 - Cursor Home
-	lcd_send_cmd(0x28); //DB5, DB3 
-	lcd_send_cmd(0x0C); //DB3, DB2
-	lcd_send_cmd(0x80); //DB3
+	LCD_send_cmd(i2cHandle, 0x01);
+	LCD_send_cmd(i2cHandle, 0x02); //DB1 - Cursor Home
+	LCD_send_cmd(i2cHandle, 0x28); //DB5, DB3 
+	LCD_send_cmd(i2cHandle, 0x0c); //
+	LCD_send_cmd(i2cHandle, 0x80); //Force cursor to 1st line
 }
 
-void LCD_send_string(char* str) {
-	while(*str) LCD_send_data(*str++);
-}w
+void LCD_send_string(I2C_HandleTypeDef i2cHandle, char* str) {
+	while(*str) {
+		putchUSART1(*str);
+		LCD_send_data(i2cHandle, *str++);
+	}
+	newlineUSART1();
+}
 
 void LCD_send_cmd(I2C_HandleTypeDef i2cHandle, char cmd) {
 	char data_u, data_l;    
 	data_u = cmd & 0xf0;
 	data_l = (cmd<<4) & 0xf0;
-	buffer[0] = data_u | 0x04;
-	buffer[1] = data_u;
-	buffer[2] = data_l | 0x04;
-	buffer[3] = data_l;
-	HAL_I2C_Master_Transmit (&i2cHandle, LCD_WRITE_ADDRESS, buffer, 4, 100);
+	buffer[0] = data_u | 0x0C;
+	buffer[1] = data_u | 0x08;
+	buffer[2] = data_l | 0x0C;
+	buffer[3] = data_l | 0x08; 
+	//buffer[0] = cmd;
+	HAL_I2C_Master_Transmit (&i2cHandle, LCD_WRITE_ADDRESS, (uint8_t *)buffer, 4, 100);
    
 }
 
 void LCD_send_data(I2C_HandleTypeDef i2cHandle, char data) {
-	char data_u, dat_l;
+	char data_u, data_l;
 	data_u = data & 0xf0;
 	data_l = (data<<4) & 0xf0;
-	buffer[0] = data_u | 0x05;
-	buffer[1] = data_u | 0x01;
-	buffer[2] = data_l | 0x05;
-	buffer[3] = data_l | 0x01;
-	HAL_I2C_Master_Transmit(&i2cHandle, LCD_WRITE_ADDRESS, buffer, 4, 100);
+	buffer[0] = data_u | 0x0D;
+	buffer[1] = data_u | 0x09;
+	buffer[2] = data_l | 0x0D;
+	buffer[3] = data_l | 0x09;
+	HAL_I2C_Master_Transmit(&i2cHandle, LCD_WRITE_ADDRESS, (uint8_t *)buffer, 4, 100);
 }
--- LCD Display/LCD_util.h
+++ LCD Display/LCD_util.h
@@ -1,4 +1,4 @@
 void LCD_init(I2C_HandleTypeDef i2cHandle);
-void LCD_send_string(char* str);
+void LCD_send_string(I2C_HandleTypeDef i2cHandle, char* str);
 void LCD_send_cmd(I2C_HandleTypeDef i2cHandle, char cmd);
 void LCD_send_data(I2C_HandleTypeDef i2cHandle, char data);
